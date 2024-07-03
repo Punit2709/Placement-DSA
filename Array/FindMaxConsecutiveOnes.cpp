5c0bffb43b64364b4a9955bfedc55b52db3ce81c
=Punit2709 <=punitsp2003@gmail.com>
2024-07-04 00:24:33 +0530

    findMaxConsecutiveOnes -- Array

---
+++ Array/FindMaxConsecutiveOnes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMaxConsecutiveOnes(vector<int> &nums){
+    int count = 0;
+    int currentCount = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++){
+        if (nums[i] == 1){
+            currentCount++;
+        }
+        else{
+            if (currentCount > count){
+                count = currentCount;
+                currentCount = 0;
+            }
+        }
+    }
+    if (currentCount > count){
+        count = currentCount;
+    }
+    return count;
+}
+
+int main()
+{
+    vector<int> nums = {1, 1, 0, 1, 1, 1};
+    int n = findMaxConsecutiveOnes(nums);
+    cout << n << " ";
+    return 0;
+}
